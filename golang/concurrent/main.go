ec7f417c2d9f320116d7bd0ce55a5da177d9537c
Anurag Kumar <kodelabzz.dev@gmail.com>
2022-08-14 23:09:53 +0530

    use of wait grp

--- golang/concurrent/main.go
+++ golang/concurrent/main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+	// "time"
+)
+
 func main() {
-	
+var wg sync.WaitGroup;
+wg.Add(10)
+words := []string { "zero","one","two","three","four","five","six","seven","eight","nine",}
+
+for i, word := range words {
+	var data string = fmt.Sprintf("%d:%s",i,word)
+	go print(data,&wg)
+}
+
+// time.Sleep(time.Second)
+
+wg.Wait()
+
+}
+
+func print(s string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Print(s)
+	fmt.Print(" ")
 }
