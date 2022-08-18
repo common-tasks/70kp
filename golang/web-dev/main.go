e4a65fece33e12edf63a6f4237016e59b3d35b39
Anurag Kumar <kodelabzz.dev@gmail.com>
2022-08-18 23:40:15 +0530

    struct of slices of struct.

--- golang/web-dev/main.go
+++ golang/web-dev/main.go
@@ -10,7 +10,7 @@ import (
 var tpl *template.Template
 
 func init() {
-	tpl = template.Must(template.ParseFiles("variable.gohtml","slice.gohtml", "map.gohtml","struct.gohtml","structslice.gohtml"))
+	tpl = template.Must(template.ParseFiles("variable.gohtml","slice.gohtml", "map.gohtml","struct.gohtml","structslice.gohtml","structslicestruct.gohtml"))
 }
 func main() {
 	printSomething("hello", "world")
@@ -24,6 +24,8 @@ func main() {
 	passStruct()
 
 	passSliceOfStruct()
+
+	passStructSliceStruct()
 }
 
 func singleVariable() {
