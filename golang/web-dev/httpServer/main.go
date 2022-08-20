3af1498ae342b39a064be5ce232bb75bc7c3ef51
Anurag Kumar <kodelabzz.dev@gmail.com>
2022-08-20 15:54:37 +0530

    basic http server.

--- golang/web-dev/httpServer/main.go
+++ golang/web-dev/httpServer/main.go
@@ -1,5 +1,22 @@
-package main 
+package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
+type myhttp int
+
+func (m myhttp) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	var msg string = " my http server"
+	fmt.Println(msg)
+	fmt.Fprintln(w,msg)
+	
+}
 func main() {
 	
+	var mhttp myhttp
+
+	http.ListenAndServe("localhost:8080", mhttp)
+
 }
