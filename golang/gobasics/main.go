4ab52ae22f419254223b9c179402550a1893b453
Anurag Kumar <kodelabzz.dev@gmail.com>
2022-08-21 17:33:42 +0530

    projects description

--- golang/gobasics/main.go
+++ golang/gobasics/main.go
@@ -6,8 +6,43 @@ import (
 )
 
 func main() {
-	//switch statement
 
+	// switchStatement()
+	// deferStatement()
+	// aboutArrays()
+	aboutSlices()
+}
+func aboutSlices() {
+	sl := make([]string, 0)
+	sl = append(sl, "hello")
+	sl = append(sl, "world")
+	sl = append(sl, "I am here")
+	sl = append(sl, "finally")
+	ln := len(sl)
+	cp := cap(sl)
+	fmt.Println(sl)
+	fmt.Println(ln, cp)
+
+}
+func aboutArrays() {
+	var a [6]string
+	a[0] = "hello"
+	a[1] = "world"
+	a[2] = "call me anytime"
+	a[3] = " and I will not pickup the call"
+	fmt.Println(a)
+}
+
+func deferStatement() {
+	fmt.Println("start")
+	for i := 0; i < 10; i++ {
+		defer fmt.Println(i)
+	}
+	fmt.Println("done")
+}
+
+// switch statement
+func switchStatement() {
 	switch os := runtime.GOOS; os {
 	case "windows":
 		{
@@ -23,5 +58,4 @@ func main() {
 		}
 
 	}
-
 }
