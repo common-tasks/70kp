e36eb4ce920bcb101e8579f947990a15cd230a0e
Anurag Kumar <kodelabzz.dev@gmail.com>
2022-08-21 21:50:46 +0530

    pointers receiver.

--- golang/gobasics/main.go
+++ golang/gobasics/main.go
@@ -5,17 +5,75 @@ import (
 	"runtime"
 )
 
+type Greetings interface {
+	Greet(msg string) string
+}
+type Mathematice interface {
+	Add(number1 int, number2 int) int
+}
+type SayHello struct {
+	Hmsg string
+}
+
+func (hello *SayHello) Greet(msg string) string {
+	hello.Hmsg = "Halo"
+	return hello.Hmsg + " " + msg
+}
+func (hello SayHello) PrintHello() {
+	fmt.Println(hello.Hmsg)
+}
+
+type Calc struct {
+}
+
+func GreetAll(g Greetings, msg string) string {
+	return g.Greet(msg)
+}
+func Addition(m Mathematice, number1 int, number2 int) int {
+	return m.Add(number1, number2)
+}
+func (cal Calc) Add(number1 int, number2 int) int {
+
+	return number1 + number2
+}
 func main() {
 
 	// switchStatement()
 	// deferStatement()
 	// aboutArrays()
 	// aboutSlices()
-	aboutMaps()
+	// aboutMaps()
+	aboutMethods()
 }
+func aboutMethods() {
+	var calculator Calc
+	result := Addition(calculator, 1, 2)
+	fmt.Println(result)
+	var hello SayHello
+	hello.Hmsg = "Hola"
+	hello.PrintHello()
+	res := GreetAll(&hello, hello.Hmsg)
+	hello.PrintHello()
+	fmt.Println(res)
 
-func aboutMaps(){
+}
+func aboutMaps() {
 
+	list := make(map[string]string)
+	list["one"] = "hello"
+	list["two"] = "world"
+	list["three"] = "arrived"
+	for _, value := range list {
+		fmt.Print(value)
+	}
+	fmt.Println()
+	delete(list, "two")
+	for _, v := range list {
+		fmt.Print(v)
+	}
+	fmt.Println()
+	elem, ok := list["one"]
+	fmt.Printf("%v is present ? %v", elem, ok)
 }
 func aboutSlices() {
 	sl := make([]string, 0)
