e4a2141a013144ecc29f196b3921277689b97200
Anurag Kumar <kodelabzz.dev@gmail.com>
2022-08-27 00:00:52 +0530

    adding bootstrap

--- reactnGo/ui/test-app/src/index.js
+++ reactnGo/ui/test-app/src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import AppFooter from './AppFooter'
+import AppContent from './AppContent'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <div className='app'>
     <App></App>
+    <AppContent></AppContent>
     <AppFooter></AppFooter>
     </div>
   </React.StrictMode>
