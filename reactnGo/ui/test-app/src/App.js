b0498f3aac8ed58ce5591163e7efe185f2fc9bf8
Anurag Kumar <kodelabzz.dev@gmail.com>
2022-08-27 08:47:48 +0530

    Simple example of events

--- reactnGo/ui/test-app/src/App.js
+++ reactnGo/ui/test-app/src/App.js
@@ -1,14 +1,18 @@
 import React,{Component} from 'react';
-import ReactDOM from 'react-dom';
 import "./App.css";
 
 
 export default class AppContent extends Component{
+  fetchList = ()=>{
+    console.log('I am clicked');
+  }
   render(){
   return (
     <div className="app">
       <div>
-      <h1>Hello World!</h1>
+      <h1>{this.props.msg1} {this.props.msg2} {this.props.msg3}</h1>
+      <br></br>
+      <button className='btn btn-primary' onClick={this.fetchList}>Fetch Data</button>
     </div>
     </div>
   );
--- reactnGo/ui/test-app/src/index.js
+++ reactnGo/ui/test-app/src/index.js
@@ -3,15 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import AppFooter from './AppFooter'
-import AppContent from './AppContent'
-import 'bootstrap/dist/css/bootstrap.css'
+import AppFooter from './AppFooter';
+import AppContent from './AppContent';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist//js/bootstrap';
+import AppHeader from './AppHeader';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const myVars= {
+  msg1:"hello",
+  msg2:"world",
+  msg3:"callme anytime and I will ignore the call"
+}
 root.render(
   <React.StrictMode>
     <div className='app'>
-    <App></App>
+    <AppHeader greetingmsg="Hello World !"></AppHeader>
+    <App {...myVars}></App>
     <AppContent></AppContent>
     <AppFooter></AppFooter>
     </div>
