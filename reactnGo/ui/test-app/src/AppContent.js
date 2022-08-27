5fa17917e214448e05beec46d4438fb4d973f71f
Anurag Kumar <kodelabzz.dev@gmail.com>
2022-08-27 08:48:31 +0530

    AppContent.js and AppHeader.js

---
+++ reactnGo/ui/test-app/src/AppContent.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+export default class AppContent extends Component{
+    render(){
+        return(
+            <p>base content 
+                <br></br>
+                <button href='#' class="btn btn-primary">Click!</button>
+            </p>
+        );
+    }
+}
