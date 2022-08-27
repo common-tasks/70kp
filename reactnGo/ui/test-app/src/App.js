db06f7ca922f3ffe4ed73855ffe3f1000569edd1
Anurag Kumar <kodelabzz.dev@gmail.com>
2022-08-27 11:31:55 +0530

    using ref

--- reactnGo/ui/test-app/src/App.js
+++ reactnGo/ui/test-app/src/App.js
@@ -2,16 +2,20 @@ import React, { Component } from "react";
 import "./App.css";
 
 export default class AppContent extends Component {
-
-  mouseMoved = () =>{
-    console.log('mouse moved');
+  constructor(props) {
+    super(props);
+    this.ulistRef = React.createRef();
   }
+  mouseMoved = () => {
+    console.log("mouse moved");
+  };
   fetchList = () => {
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then((response) => response.json())
       .then((json) => {
-        var ulist = document.getElementById('ulist');
-        json.forEach(element => {
+        // var ulist = document.getElementById('ulist');
+        const ulist = this.ulistRef.current;
+        json.forEach((element) => {
           let list = document.createElement("li");
           list.appendChild(document.createTextNode(element.title));
           ulist.appendChild(list);
@@ -34,7 +38,7 @@ export default class AppContent extends Component {
         <br></br>
         <hr></hr>
         <p onMouseOut={this.mouseMoved}>para1</p>
-        <ul id="ulist"></ul>
+        <ul id="ulist" ref={this.ulistRef}></ul>
       </div>
     );
   }
