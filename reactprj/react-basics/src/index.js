459ed35602bb51162a62d9e1ea63e7aa770f64b8
Anurag Kumar <kodelabzz.dev@gmail.com>
2022-08-31 01:21:46 +0530

    cleanup...

--- reactprj/react-basics/src/index.js
+++ reactprj/react-basics/src/index.js
@@ -1,20 +1,32 @@
 import React, { Fragment } from "react";
 import ReactDOM from "react-dom";
-import './index.css';
+import "./index.css";
 
-function Greeting() {
-  return (
-    <Fragment>
-      <NameOfPerson />
-      <Message />
-    </Fragment>
+const Book = () => {
+  const authorDetails = {
+    authorNamewa:'kunar',
+    age:50
+  }
+  return(
+  <Fragment>
+     <h1>Book01</h1>
+     <AuthorName uthorName={authorDetails.authorNamewa} age ={authorDetails.age}/>
+     <AuthorAge age={authorDetails.age}/>
+  </Fragment>
+  );
+};
+const AuthorAge =(props) =>{
+  return(
+    <h1>{props.age}</h1>
+  );
+}
+const AuthorName =(props) =>{
+  return(
+<Fragment>
+  <h1>{props.uthorName}</h1>
+
+</Fragment>
   );
 }
 
-const NameOfPerson = () => {
-  return <h1>James Mahto</h1>;
-};
-const Message = () => {
-  return <h1>Sab ko parnam</h1>;
-};
-ReactDOM.render(<Greeting />, document.getElementById("root"));
+ReactDOM.render(<Book />, document.getElementById("root"));
