beca2220e8920050d125ed302658030019e3b2b6
Anurag Kumar <kodelabzz.dev@gmail.com>
2022-08-31 23:01:00 +0530

    useState hook example.

--- reactprj/react-advanced/src/tutorial/1-useState/setup/1-error-example.js
+++ reactprj/react-advanced/src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  const [title, setTitle] = useState("random title");
+  const handleClick = () => {
+    if (title === "random title") {
+      setTitle("hello world");
+    } else {
+      setTitle("random title");
+    }
+  };
+  return (
+    <React.Fragment>
+      <h1>{title}</h1>
+      <button type="button" className="btn" onClick={handleClick}>
+        Change Title
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default ErrorExample;
