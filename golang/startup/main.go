0fcbfe18c1cc9047444103fa25a2f59d199172cd
Anurag Kumar <kodelabzz.dev@gmail.com>
2022-09-24 14:50:35 +0530

    tic-tac-toe game.

--- golang/startup/main.go
+++ golang/startup/main.go
@@ -4,6 +4,7 @@ import
 (
 	"fmt"
 	"rsc.io/quote"
+	
 )
 
 func main(){
@@ -11,3 +12,10 @@ func main(){
 	fmt.Println(quote.Go())
 }
 
+// album represents data about a record album.
+type album struct {
+    ID     string  `json:"id"`
+    Title  string  `json:"title"`
+    Artist string  `json:"artist"`
+    Price  float64 `json:"price"`
+}
--- reactprj/react-advanced/src/data.js
+++
@@ -1,6 +0,0 @@
-export const data = [
-  { id: 1, name: 'john' },
-  { id: 2, name: 'puttar' },
-  { id: 3, name: 'sushasan' },
-  { id: 4, name: 'anna' },
-];
