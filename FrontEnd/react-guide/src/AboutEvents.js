3b5b6d392550ae2556b982503f0a6e714b735506
Anurag Kumar <kodelabzz.dev@gmail.com>
2022-10-04 22:31:33 +0530

    rest apis in go

--- FrontEnd/react-guide/src/AboutEvents.js
+++ FrontEnd/react-guide/src/AboutEvents.js
@@ -2,11 +2,16 @@ import React from "react";
 
 class AboutEvents extends React.Component{
 
-    
+    HandleButtonClick = (e)=>{
+        e.preventDefault();
+        console.log('I am clicked' +e);
+    }
+
 render() {
     return (
          <div>
             <h1>Handling events</h1>
+            <button onClick={this.HandleButtonClick} >click</button>
          </div>
     );
 }
--- FrontEnd/react-guide/src/App.js
+++ FrontEnd/react-guide/src/App.js
@@ -1,29 +1,17 @@
 import "./App.css";
 import "./AboutEvents";
 import AboutEvents from "./AboutEvents";
-
-const user = {
-  name: "james khalkho",
-  job: "full time",
-};
-const name = "murmu kumar";
+import Toggle from "./Toggle";
+import ConditionalRendereing from "./ConditionalRendering";
 
 function App() {
   return (
     <div className="App">
-      {/* <h1>
-        hello {name} and {formatName(user)}
-      </h1>
-      <GetName name="kunar" />
-      <GetName name="gammu" /> */}
-    <AboutEvents />
+      <AboutEvents />
+      <Toggle />
+      <ConditionalRendereing isLoggedIn = {false}/>
     </div>
   );
 }
-function formatName(user) {
-  return user.name + "::" + user.job;
-}
-function GetName(props) {
-  return <h1>{props.name}</h1>;
-}
+
 export default App;
