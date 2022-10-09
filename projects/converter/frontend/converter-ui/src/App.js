2ab76a40be359d0fe6f6251304e1af37bf14088f
Anurag Kumar <kodelabzz.dev@gmail.com>
2022-10-09 20:51:09 +0530

    3 column layout with column height fits to browser height using height: 100vh;

--- projects/converter/frontend/converter-ui/src/App.js
+++ projects/converter/frontend/converter-ui/src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
+    <div className="row">
+      <div className="column left">
+        <h2>Column 1</h2>
+        <p>Some text..</p>
+      </div>
+      <div className="column middle" >
+        <h2>Column 2</h2>
+        <p>Some text..</p>
+      </div>
+      <div className="column right">
+        <h2>Column 3</h2>
+        <p>Some text..</p>
+      </div>
     </div>
   );
 }
