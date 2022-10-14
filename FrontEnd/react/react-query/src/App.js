77ac575d7a52730b7e4ca5eb058154f5e6dedacd
Anurag Kumar <kodelabzz.dev@gmail.com>
2022-10-14 21:05:49 +0530

    Implementing a get rest API call using axiom and react-query.

--- FrontEnd/react/react-query/src/App.js
+++ FrontEnd/react/react-query/src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import fetchPosts from "./FetchAPI";
+import { useQuery } from "react-query";
 
 function App() {
+  const { isLoading, isFetching, error, data, status } = useQuery(
+    "users",
+    fetchPosts
+  );
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error! {error.message}</div>;
+  }
+
+  if (isFetching) {
+    return <div>Fetching data...</div>;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="container">Users Name</h1>
+      {data.map((users, id) => {
+        return (
+          <li className="container" key={id}>
+            {users.name}
+          </li>
+        );
+      })}
+      <h4>{status}</h4>
     </div>
   );
 }
