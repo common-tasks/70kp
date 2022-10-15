edeff9a07f30353d48ce9f474657e3e1f74356e8
Anurag Kumar <kodelabzz.dev@gmail.com>
2022-10-16 01:42:54 +0530

    handle specific get request (request by id)

--- projects/common-libraries/backend/main.go
+++ projects/common-libraries/backend/main.go
@@ -22,14 +22,19 @@ func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/health-check", HealthCheck).Methods("GET")
 	router.HandleFunc("/allServices", AllServices)
+	router.HandleFunc("/service/{name}", SingleService)
+
 	var port string = ":9999"
 	fmt.Println("starting server")
+	
 	c := cors.New(cors.Options{
 		AllowedOrigins:   []string{"*"},
 		AllowCredentials: true,
 	})
+	
 	handler := c.Handler(router)
 	//start and listen to the request
+	
 	http.ListenAndServe(port, handler)
 
 }
@@ -46,6 +51,19 @@ func AllServices(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(GetServices())
 
 }
+func SingleService(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("End point hit: specific service")
+	vars:=mux.Vars(r)
+	name:=vars["name"]
+	for _, service := range GetServices() {
+		if(service.ServiceName==name){
+			json.NewEncoder(w).Encode(service)
+		}
+	}
+	w.WriteHeader(http.StatusOK)
+
+}
+
 func GetServices() []Service {
 	services := []Service{}
 	var service1 Service
