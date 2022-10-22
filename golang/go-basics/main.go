81177056c38127102a7d5899222b056a62642ce7
Anurag Kumar <kodelabzz.dev@gmail.com>
2022-10-22 16:45:58 +0530

    continued...

--- golang/go-basics/main.go
+++ golang/go-basics/main.go
@@ -4,6 +4,10 @@ import "fmt"
 
 func main() {
 	var x int =44
-	fmt.Printf("%d %p",x,&x)
+	fmt.Printf("%d %p\n",x,&x)
+	var intPtr *int = &x;
+	*intPtr++;
+	fmt.Printf("%v\n",*intPtr)
+	fmt.Printf("%v\n",x)
 
 }
