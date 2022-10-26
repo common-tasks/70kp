5a9b8999520118db0448648a814b0355cd8d9230
Anurag Kumar <kodelabzz.dev@gmail.com>
2022-10-26 13:25:12 +0530

    binary search logic on a sorted array

--- golang/go-basics/arrays.go
+++ golang/go-basics/arrays.go
@@ -1,6 +1,5 @@
 package main
 
-
 func SumOfArray(arr []int) int {
 
 	var sum int
@@ -10,12 +9,29 @@ func SumOfArray(arr []int) int {
 	return sum
 }
 
-func LinearSearch(arr []int,number int) bool {
-	var isFound bool =false
+func LinearSearch(arr []int, number int) bool {
+	var isFound bool = false
 	for i := 0; i < len(arr); i++ {
-		if(arr[i]==number){
-			isFound=true
+		if arr[i] == number {
+			isFound = true
 			break
+		}
+	}
+	return isFound
+}
+func BinarySearch(sarray []int, item int) bool {
+	var isFound bool = false
+	length := len(sarray)
+	low := 0
+	high := length - 1
+	for low <= high {
+		mid := (low + high) / 2
+		if item == sarray[mid] {
+			return true
+		} else if item < sarray[mid] {
+			high = mid - 1
+		} else {
+			low = mid + 1
 		}
 	}
 	return isFound
--- golang/go-basics/main.go
+++ golang/go-basics/main.go
@@ -7,13 +7,17 @@ func main() {
 	var arr []int
 	arr = append(arr, 2, 4, 6, 8)
 	result := SumOfArray(arr)
-	fmt.Printf("sum of input array is  %d ", result)
-	arr2:=[]int{1,2,3,4}
-	fmt.Printf("sum of input array2 is %d",SumOfArray(arr2))
+	fmt.Printf("sum of input array is  %d\n ", result)
+	arr2 := []int{1, 2, 3, 4}
+	fmt.Printf("sum of input array2 is %d\n", SumOfArray(arr2))
 
-	arr3:=[]int{1,2,3,4,5}
-	fmt.Printf("number found? %t",LinearSearch(arr3,11))
+	arr3 := []int{1, 2, 3, 4, 5}
+	fmt.Printf("number found? %t\n", LinearSearch(arr3, 11))
 
+	sortedarray := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	element:=20
+	found := BinarySearch(sortedarray, element)
+	fmt.Printf("element %d found in BinarySearch ? %t\n", element,found)
 
 }
 
