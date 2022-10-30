b9121448d606255b7f3ed1fad311f7c2381338a9
Anurag Kumar <kodelabzz.dev@gmail.com>
2022-10-30 16:10:12 +0530

    rotate an element by k position problem.

---
+++ golang/workspace/src/lib/dsa/arrays.go
@@ -0,0 +1,11 @@
+package dsa
+
+import 
+(
+	"fmt"
+)
+
+func RotateArrayByK(k int){
+	fmt.Printf("%s","rotate an element by K positions")
+	
+}
---
+++ golang/workspace/src/main/main.go
@@ -0,0 +1,12 @@
+package main
+
+import
+(
+	"go-workspace/src/lib/dsa"
+	"fmt"
+)
+
+func main(){
+	fmt.Println("main method entry")
+	dsa.RotateArrayByK(2)
+}
